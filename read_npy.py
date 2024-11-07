fa58b5523e4f14ee9123bf8c0599976802629415
arul <arul@ip-10-100-13-118.ec2-east.lightmatter.co>
2024-11-07 22:11:47 +0000

    add examples

--- read_npy.py
+++ read_npy.py
@@ -5,6 +5,8 @@ if __name__ == "__main__":
     file_name = sys.argv[1]
     arr = np.load(file_name) * -1
     arr = arr.astype(np.uint32)
+    if "hops" in file_name:
+        arr = arr.reshape(arr.shape[0] // arr.shape[1], -1, arr.shape[-1])
     print(arr.shape)
     print(arr.dtype)
     print(arr)
